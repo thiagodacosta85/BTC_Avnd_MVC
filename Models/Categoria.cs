4ac7d1a70e271155101c6f9af8f264d136f62342
thiagodacosta85 <thiago.costa85@outlook.com>
2021-01-06 16:22:14 -0300

    Aula_01

---
+++ Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BTC_Avnd_MVC.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
